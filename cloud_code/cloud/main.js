3cba70e613956a66ba5b8ca5a3d401f07c010dd6
Igor_Ribeiro <ribeiro_igor@hotmail.com>
2017-10-03 15:06:52 -0300

    Adding pushtest job

--- cloud_code/cloud/main.js
+++ cloud_code/cloud/main.js
@@ -4,3 +4,21 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!KWEFBBKEKBEFKBKRBJ");
 });
+
+
+Parse.Cloud.job("pushtest", function(request, response) {
+    Parse.Push.send({
+        channels: [ "" ],
+        data: {
+            alert: "Pushtest From Job"
+        }
+        }, {
+        useMasterKey: true,
+        success: function() {
+            console.log("Pushed");
+        // success!
+        }, error: function(err) {
+            console.log(err);
+        }
+    });
+});
