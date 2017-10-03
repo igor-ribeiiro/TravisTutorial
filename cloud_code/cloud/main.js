eda7f8b2b051ecf868c90c0dbf4abfcba20e03cd
Igor_Ribeiro <ribeiro_igor@hotmail.com>
2017-10-03 15:55:38 -0300

    Changing push notification name

--- cloud_code/cloud/main.js
+++ cloud_code/cloud/main.js
@@ -1,11 +1,21 @@
-Parse.Cloud.job("jobNotification", function(request, response) {
+Parse.Cloud.define("jobPushNotification", function (request, response) {
     Parse.Push.send({
-        useMasterKey: true,
-        success: function() {
-            console.log("The job notification is running!");
-        // success!
-        }, error: function(err) {
-            console.log(err);
+        channels: ["Your Channel name here"],
+        data: {
+            title: "Hello!",
+            message: "Hello from the Cloud Code",
         }
-    });
+        }, {
+        success: function () {
+            // Push was successful
+            response.sucess("push sent");
+            console.log("The job notification is running!");
+        },
+        error: function (error) {
+        // Push was unsucessful
+            response.sucess("error with push: " + error);
+            console.log(error);
+        },
+        useMasterKey: true
+   });
 });
