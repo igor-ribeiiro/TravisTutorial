4e2733390db9ced1b5ecc743ee0d6c49d6b57389
Igor_Ribeiro <ribeiro_igor@hotmail.com>
2017-10-03 15:45:51 -0300

    Adding masterkey

--- cloud_code/cloud/main.js
+++ cloud_code/cloud/main.js
@@ -1,5 +1,6 @@
 Parse.Cloud.job("jobNotification", function(request, response) {
     Parse.Push.send({
+        useMasterKey: true,
         success: function() {
             console.log("The job notification is running!");
         // success!
