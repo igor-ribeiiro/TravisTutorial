5658a85427e48e00ef61d11b0717c511b4fe9218
Igor_Ribeiro <ribeiro_igor@hotmail.com>
2017-10-03 15:22:18 -0300

    Simplifing job

--- app/src/main/java/com/example/root/travis/MainActivity.java
+++ app/src/main/java/com/example/root/travis/MainActivity.java
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +26,21 @@ public class MainActivity extends AppCompatActivity {
                 .clientKey(CLIENT_KEY)
                 .server(BACK4APP_API).build()
         );
+
+        // Defines the channels this should listen to
+        LinkedList<String> channels = new LinkedList<String>();
+
+        // Pushes channelName
+        channels.push("channelName");
+
+        // Defines ParseInstallation
+        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+
+        // Adds the Firebase Sender Key
+        installation.put("GCMSenderId", "<Your Firebase Sender Key>");
+
+        // Adds the channel list to the installation
+        installation.put("channels", channels);
+        installation.saveInBackground();
     }
 }
--- cloud_code/cloud/main.js
+++ cloud_code/cloud/main.js
@@ -1,21 +1,7 @@
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!KWEFBBKEKBEFKBKRBJ");
-});
-
-
-Parse.Cloud.job("pushtest", function(request, response) {
+Parse.Cloud.job("jobNotification", function(request, response) {
     Parse.Push.send({
-        channels: [ "" ],
-        data: {
-            alert: "Pushtest From Job"
-        }
-        }, {
-        useMasterKey: true,
         success: function() {
-            console.log("Pushed");
+            console.log("The job notification is running!");
         // success!
         }, error: function(err) {
             console.log(err);
