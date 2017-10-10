9dd31c71ef67f1a30c0731b48cd7612f9f7f9c2b
Igor Ribeiro <ribeiro_igor@hotmail.com>
2017-10-10 16:01:53 -0300

    Putting comments on th db test

--- spec/helpers/ParseInitialize.js
+++ spec/helpers/ParseInitialize.js
@@ -1,31 +1,31 @@
-const path = require('path');
-const express = require('express');
-const ParseServer = require('parse-server').ParseServer;
-const Parse = require('parse/node');
-const app = express();
-const TestUtils = require('parse-server').TestUtils;
-
-beforeAll((done) => {
-  const api = new ParseServer({
-    databaseURI: 'mongodb://localhost:27017/integration',
-    appId: 'integration',
-    masterKey: 'notsosecret',
-    serverURL: 'http://localhost:1337/',
-    cloud: path.join(__dirname, '../../cloud_code/cloud/main.js'),
-    logLevel: '0'
-  });
-  app.use('/parse', api);
-  app.get('/clear', (req, res) => {
-    TestUtils.destroyAllDataPermanently().then(() => {
-      res.send('{}');
-    });
-  });
-
-  Parse.initialize('integration', null, 'notsosecret');
-  Parse.CoreManager.set('SERVER_URL', 'http://localhost:1337/parse');
-  Parse.Storage._clear();
-
-  app.listen(1337, () => {
-    done();
-  });
-});
+//const path = require('path');
+//const express = require('express');
+//const ParseServer = require('parse-server').ParseServer;
+//const Parse = require('parse/node');
+//const app = express();
+//const TestUtils = require('parse-server').TestUtils;
+//
+//beforeAll((done) => {
+//  const api = new ParseServer({
+//    databaseURI: 'mongodb://localhost:27017/integration',
+//    appId: 'integration',
+//    masterKey: 'notsosecret',
+//    serverURL: 'http://localhost:1337/',
+//    cloud: path.join(__dirname, '../../cloud_code/cloud/main.js'),
+//    logLevel: '0'
+//  });
+//  app.use('/parse', api);
+//  app.get('/clear', (req, res) => {
+//    TestUtils.destroyAllDataPermanently().then(() => {
+//      res.send('{}');
+//    });
+//  });
+//
+//  Parse.initialize('integration', null, 'notsosecret');
+//  Parse.CoreManager.set('SERVER_URL', 'http://localhost:1337/parse');
+//  Parse.Storage._clear();
+//
+//  app.listen(1337, () => {
+//    done();
+//  });
+//});
